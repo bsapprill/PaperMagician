f4b332f1585acddb3ec857988ff0410918e29ac6
BenApprill <bsapprill@fullsail.edu>
2017-11-02 05:29:47 -0500

    Added code for dripping ceiling

---
+++ CeilingDrip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingDrip : MonoBehaviour {
+	public float fallingSpeed;
+	public int dripDamage;
+
+	void Update(){
+		transform.position += fallingSpeed * Vector3.down * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			//Is negative to represent the health "change"
+			PlayerHealth.PlayerHealthChange (-dripDamage);
+
+			Destroy (this.gameObject);
+		}
+		else if(other.gameObject.layer == 8){
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ DripCeiling.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DripCeiling : MonoBehaviour {
+
+	#region Variables
+
+	public GameObject dripPrefab;
+
+	public float timeBetweenDrips;
+	float dripTimer = 0f;
+
+	Vector3 dripSpawn;//Where the drips instantiate
+	
+	#endregion
+
+	void Start(){
+		Vector3 dripSpawn = CalculateDripSpawn ();
+	}
+
+	void Update(){
+		dripTimer += Time.deltaTime;
+
+		if(dripTimer > timeBetweenDrips){
+			dripTimer = 0f;
+			Instantiate (dripPrefab, dripSpawn, Quaternion.identity);
+		}
+	}
+
+	#region Functions
+
+	//May be expanded later
+	Vector3 CalculateDripSpawn(){
+		Vector3 centerOfCeilingDrip = Vector3.zero;
+
+		centerOfCeilingDrip.x = transform.position.x;
+
+		return centerOfCeilingDrip;
+	}
+
+	#endregion
+}
